9388c1368dcee49cdbcdc5797c811743ebc88757
Ivansquark <squark@mail.ru>
2022-04-18 11:43:17 +0300

    +instruction fix

--- instruction/package/rtl8723ds/rtl8723ds.mk
+++ instruction/package/rtl8723ds/rtl8723ds.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-RTL8723DS_SITE = "https://github.com/Ivansquark/rtl8732ds_riscv"
-RTL8723DS_LICENSE = GPL-2.0
+RTL8723DS_VERSION = d9adde91b18553d6363e6da2cdfe3910e05c333e
+RTL8723DS_SITE = $(call github,Ivansquark,rtl8723ds_riscv,$(RTL8723DS_VERSION))
+RTL8723DS_LICENSE = GPL-2.0, proprietary (*.bin firmware blobs)
 
 RTL8723DS_MODULE_MAKE_OPTS = \
   CONFIG_RTL8723DS=m \
